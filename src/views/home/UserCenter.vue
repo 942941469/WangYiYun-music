4ffb9699b7b98f7d4ff77e46a480b60cc5807c0d
LiJianing <942941469@qq.com>
2023-01-17 12:07:58 +0800

    个人中心修改

--- src/views/home/UserCenter.vue
+++ src/views/home/UserCenter.vue
@@ -14,19 +14,24 @@ const profile = computed(() => store.state.footer.profile)
 
 <style lang="less" scoped>
 .user{
-  width: 100%;
-  height: 2rem;
+  width: 70%;
+  height: 2.5rem;
   display: flex;
   flex-direction: column;
-  margin-top: .7rem;
+  align-items: center;
+  margin: .5rem auto;
+  border-radius: .7rem;
+  background-color: red;
   img{
     width: 1rem;
     height: 1rem;
     border-radius: 50%;
+    margin: .3rem auto;
   }
   .name{
     font-weight: 700;
     font-size: .4rem;
+    text-align: center;
   }
 }
 </style>
